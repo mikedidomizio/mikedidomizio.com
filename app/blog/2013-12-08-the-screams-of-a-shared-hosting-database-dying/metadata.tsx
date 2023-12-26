7165f6c10cebd4bbd85696ec68e8ea0b0e96886b
Mike DiDomizio <mikedidomizio@gmail.com>
2023-12-26 06:35:56 -0500

    Create metadata to have a central place

---
+++ app/blog/2013-12-08-the-screams-of-a-shared-hosting-database-dying/metadata.tsx
@@ -0,0 +1,10 @@
+import sharedDatabase from "./shared-database.png";
+
+export const metadata = {
+    date: 'December 8th, 2013',
+    picture: <img src={sharedDatabase.src}
+    alt="A picture of a database table fetching rows" />,
+    tags: ['bots', 'drupal', 'mysql', 'sql'],
+    title: 'The screams of a shared hosting database dying',
+    url: '/blog/2013-12-08-the-screams-of-a-shared-hosting-database-dying'
+}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,9 @@
-import mideoMeDiscover from './2013-11-20-building-my-first-phonegap-ios-app-and-what-i-learned/mideome-discover-new-people.jpeg'
-import sharedDatabase from './2013-12-08-the-screams-of-a-shared-hosting-database-dying/shared-database.png'
-
 import {Navbar} from "@/app/components/Navbar";
 import {Content} from "@/app/components/Content";
 import {BlogPostCard} from "@/app/components/BlogPostCard";
+
+import { metadata as sharedDatabaseMetadata } from './2013-12-08-the-screams-of-a-shared-hosting-database-dying/metadata'
+import { metadata as mideoMeIOS } from './2013-11-20-building-my-first-phonegap-ios-app-and-what-i-learned/metadata'
 
 export default function Page() {
   return (
@@ -12,40 +12,16 @@ export default function Page() {
         <Content>
           <h2 className="text-4xl font-bold mb-4">Blog Posts</h2>
           <div className="flex flex-wrap -mx-2">
-              <BlogPostCard date="December 8th 2013"
-                            link="/blog/2013-12-08-the-screams-of-a-shared-hosting-database-dying"
-                            image={<img src={sharedDatabase.src}
-                                        alt="A picture of a database table fetching rows" />}
-                            title="The screams of a shared database dying" tags={['bots', 'drupal', 'mysql', 'sql']} />
+              <BlogPostCard date={sharedDatabaseMetadata.date}
+                            link={sharedDatabaseMetadata.url}
+                            image={sharedDatabaseMetadata.picture}
+                            title={sharedDatabaseMetadata.title} tags={sharedDatabaseMetadata.tags} />
 
+              <BlogPostCard date={mideoMeIOS.date}
+                          link={mideoMeIOS.url}
+                          image={mideoMeIOS.picture}
+                          title={mideoMeIOS.title} tags={mideoMeIOS.tags} />
 
-              <BlogPostCard date="November 20th 2013"
-                          link="/blog/2013-11-20-building-my-first-phonegap-ios-app-and-what-i-learned"
-                          image={<img className="h-[300px]" src={mideoMeDiscover.src}
-                          alt="MideoMe in the app store" />}
-                          title="Building my first Phonegap iOS app and what I learned" tags={['app', 'codeigniter', 'iphone', 'iphone app', 'mideome', 'osx', 'phonegap']} />
-
-              {/*<div className="w-full md:w-1/3 px-2">*/}
-            {/*  <Link href="/blog/2013-11-20-building-my-first-phonegap-ios-app-and-what-i-learned">*/}
-            {/*    <div className="card w-96 bg-base-100 shadow-xl">*/}
-            {/*      <figure><img className="h-[300px]" src={mideoMeDiscover.src} alt="MideoMe in the app store" /></figure>*/}
-            {/*      <div className="card-body">*/}
-            {/*        <h2 className="card-title">*/}
-            {/*          Building my first Phonegap iOS app and what I learned*/}
-            {/*          /!*<div className="badge badge-secondary">NEW</div>*!/*/}
-            {/*        </h2>*/}
-            {/*        <p>November 20th 2013</p>*/}
-            {/*        <div className="card-actions justify-end">*/}
-            {/*          <div className="badge badge-outline">iOS</div>*/}
-            {/*          <div className="badge badge-outline">iPhone</div>*/}
-            {/*          <div className="badge badge-outline">OSX</div>*/}
-            {/*          <div className="badge badge-outline">CodeIgniter</div>*/}
-            {/*          <div className="badge badge-outline">MideoMe</div>*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </Link>*/}
-            {/*</div>*/}
           </div>
         </Content>
       </div>
