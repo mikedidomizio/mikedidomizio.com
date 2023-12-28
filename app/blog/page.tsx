c052d8ae93a18b670de8eec6b2403218ba6df1fb
Mike DiDomizio <mikedidomizio@gmail.com>
2023-12-28 17:46:43 -0500

    Add post for Shopify Javascript API

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,7 @@ import {BlogPostCard} from "@/app/components/BlogPostCard";
 
 import { metadata as sharedDatabaseMetadata } from './2013-12-08-the-screams-of-a-shared-hosting-database-dying/metadata'
 import { metadata as mideoMeIOS } from './2013-11-20-building-my-first-phonegap-ios-app-and-what-i-learned/metadata'
+import { metadata as shopifyApi } from './2013-12-16-shopify-javascript-api/metadata'
 
 export default function Page() {
   return (
@@ -12,6 +13,11 @@ export default function Page() {
         <Content>
           <h2 className="text-4xl font-bold mb-4">Blog Posts</h2>
           <div className="grid gap-4 lg:grid-cols-3">
+              <BlogPostCard date={shopifyApi.date}
+                            link={shopifyApi.url}
+                            image={shopifyApi.picture}
+                            title={shopifyApi.title} tags={shopifyApi.tags} />
+
               <BlogPostCard date={sharedDatabaseMetadata.date}
                             link={sharedDatabaseMetadata.url}
                             image={sharedDatabaseMetadata.picture}
