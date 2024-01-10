463b15ea5f4d538e5d24d70ec1e090e99cc33dca
Mike DiDomizio <mikedidomizio@gmail.com>
2024-01-09 23:19:33 -0500

    Update About Me section with who I am and pictures (#3)

---
+++ components/YouTube.tsx
@@ -0,0 +1,10 @@
+export default function YouTube({ id }: { id: string }) {
+  return (
+    <iframe
+      className="aspect-video w-full"
+      src={`https://www.youtube.com/embed/${id}`}
+      title="YouTube Video Player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    />
+  )
+}
