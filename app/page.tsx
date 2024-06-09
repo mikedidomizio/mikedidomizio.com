2774a92eeeb13d20bf532b1c4030bb562dee3708
Mike DiDomizio <mikedidomizio@gmail.com>
2024-06-09 16:04:55 -0400

    Add height to page

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,5 @@ import Main from './Main'
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return (
-    <>
-      embed drive
-      <embed
-        src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://mikedidomizio-com-dev-mode.vercel.app/static/semantic-versioning/SemanticVersioning.pdf"
-        width="100%"
-        height={400}
-      />
-    </>
-  )
+  return <Main posts={posts} />
 }
