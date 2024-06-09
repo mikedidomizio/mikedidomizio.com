511f6c56a12eaafc5430d42116cbf120aca6322c
Mike DiDomizio <mikedidomizio@gmail.com>
2024-06-09 12:24:58 -0400

    Test semantic versioning in MDX and React

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,26 @@ import Main from './Main'
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  return (
+    <>
+      <object
+        data="/static/semantic-versioning/SemanticVersioning.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      >
+        Could not load PDF - Here is a{' '}
+        <a href="/static/semantic-versioning/SemanticVersioning.pdf">link to the PDF!</a>
+      </object>
+
+      <embed
+        src="/static/semantic-versioning/SemanticVersioning.pdf"
+        width="500"
+        height="375"
+        type="application/pdf"
+      />
+
+      <iframe src="/static/semantic-versioning/SemanticVersioning.pdf"></iframe>
+    </>
+  )
 }
