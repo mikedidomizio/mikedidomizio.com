9f76418119d21a0e937fc232d9bf007fa5d39a35
Mike DiDomizio <mikedidomizio@gmail.com>
2024-11-13 18:45:38 -0500

    Add comment on what the route is for

--- app/.well-known/atproto-did/route.ts
+++ app/.well-known/atproto-did/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+// The purpose of this is identity verification across AT Protocol platforms like BlueSky
+// https://docs.bsky.app/docs/advanced-guides/atproto
 export async function GET() {
   const textFileContents = 'did:plc:clt4j4uhlrdlm5j6piuxxddw';
 
