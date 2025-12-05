db7734f14892ef8f42b137ea46ed5dcb8a7015a4
Mike DiDomizio <mikedidomizio@gmail.com>
2025-12-05 12:36:41 -0500

    Fix atproto file

---
+++ app/api/.well-known/atproto-did/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+// The purpose of this is identity verification across AT Protocol platforms like BlueSky
+// https://docs.bsky.app/docs/advanced-guides/atproto
+export async function GET() {
+    const textFileContents = 'did:plc:clt4j4uhlrdlm5j6piuxxddw';
+
+    return new NextResponse(textFileContents, {
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+    });
+}
